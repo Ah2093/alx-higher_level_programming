35627523734215b37b49702531b753ed7c93f80c
ahmed hamdi <ah2093@fayoum.edu.eg>
2023-03-18 22:14:54 -0700

    all

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 10-add.py
+#ahmed hamdi <ahmedhamdy20p1@gmailcom>
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 11-pow.py
+#ahmed hamdi <ahmedhamdy20p1@gmailcom>
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
